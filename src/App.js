01315ae36dbea763e3ab9a6a66103eb49dfe6aa9
Dmitry Revyakin <nikulincorp@icloud.com>
2019-03-16 20:23:58 +0200

    install bootstrap 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+
+import MainPage from "./MainPage";
+import "./App.css";
 
 class App extends Component {
-    render(){
-        return <div> Hello </div>
-    }
+  render() {
+    return (
+      <div className="App">
+        <MainPage />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/MainPage.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class MainPage extends Component {
+    render(){
+        return (
+            <h1>Добро пожаловать в ТЦ Горьгоко 55!</h1>
+        )
+    }
+}

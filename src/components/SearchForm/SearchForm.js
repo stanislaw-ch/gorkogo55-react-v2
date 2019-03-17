154e4504838a8cd5b7c1b2acf5df4a3095a88c65
Vadim Cpp <vadim.cpp@gmail.com>
2019-03-17 18:49:32 +0200

    Refactored SearchForm component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,14 +1,31 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 const searchForm = props => {
   const { onSearchForm } = props;
   return (
-    <form onSubmit={onSearchForm}>
-      <label>
-        <input type="text" />
-      </label>
-      <input type="submit" value="Найти" />
-    </form>
+    <Container>
+      <Row>
+        <Col>
+          <Form.Control
+            type="text"
+            placeholder="Что найти?"
+          />
+        </Col>
+        <Col md="auto">
+          <Button
+            variant="primary"
+            onClick={ () => onSearchForm() }
+          >
+            Найти
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 

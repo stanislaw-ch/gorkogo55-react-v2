b39e26c05401da4e1806a101ace9c0a3c79f8463
Dmitry Revyakin <nikulincorp@icloud.com>
2019-03-17 14:38:23 +0200

    add information about us

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,9 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component, Fragment } from "react";
 
 export default class MainPage extends Component {
-    render(){
-        return (
-            <h1>Добро пожаловать в ТЦ Горьгоко 55!</h1>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <h1>Добро пожаловать в ТЦ Горьгоко 55!</h1>
+        <p>
+          Мы собрали на нашем сайте всю информацию о магазинах торгового центра.
+          Введите в поисковое поле номер телефона, название магазина, или товар
+          который вы хотите приобрести
+        </p>
+      </Fragment>
+    );
+  }
 }

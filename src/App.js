1e12abde27e062fa6e271b24b3ee927e971e355c
Vadim Cpp <vadim.cpp@gmail.com>
2019-03-23 15:52:43 +0200

    Read real shops data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
+import Tabletop from 'tabletop';
 
 import MainPage from "./containers/MainPage/MainPage";
 import SearchPage from "./containers/SearchPage/SearchPage";
@@ -7,11 +8,31 @@ import ShowPage from "./containers/ShopPage/ShopPage";
 import testData from "./data/shops.json";
 import "./App.css";
 
+//
+// NOTE!
+// Google Sheets is being read with the help of this article:
+// https://medium.com/@ryan.mcnierney/using-react-google-sheets-as-your-cms-294c02561d59
+//
+const spreadSheetKey = '1rg0Wkb4E1MccFnNJcasmn4uUwxNXDOs_ObeOC9MyYiM';
+
 class App extends Component {
   state = {
     testData,
-    searchValue: ""
+    searchValue: "",
+    data: testData,
   };
+
+  componentDidMount() {
+    Tabletop.init({
+      key: spreadSheetKey,
+      callback: googleData => {
+        console.log('google sheet data --->', googleData);
+        this.setState({data: googleData});
+      },
+      simpleSheet: true
+    })
+  }
+
   changeSearchValue = event => {
     event.preventDefault();
     this.setState({ searchValue: event.target.value });

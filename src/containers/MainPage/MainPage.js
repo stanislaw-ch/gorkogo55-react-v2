d42e8b39d144a4b09b9def87d99bdbe79cf9d4c2
Dmitry Revyakin <dm1tryrev@yandex.ru>
2019-04-20 18:54:23 +0300

    delete img in mainpage component

--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -2,43 +2,14 @@ import React, { Component } from "react";
 import { Button } from "react-bootstrap";
 import { FaSearch } from "react-icons/fa";
 import { FiPhone, FiAlertCircle } from "react-icons/fi";
-import background from "../../assets/images/background.jpg";
 
 // import Spinner from "../../components/Spinner/Spinner";
 import "./MainPage.css";
 
 export default class MainPage extends Component {
-  state = {
-    displayWidth: 0,
-    displayHeight: 0,
-    imgLeft: 0,
-    imgTop: 0
-  };
-
   componentDidMount() {
     this.props.clearSearchValue();
-    this.updateWindowDimensions();
-    window.addEventListener("resize", this.updateWindowDimensions);
   }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateWindowDimensions);
-  }
-
-  updateWindowDimensions = () => {
-    let imgLeft = 0;
-    let imgTop = 0;
-    if (window.innerWidth < 1200) {
-      imgLeft = 960 - window.innerWidth;
-      imgTop = 530 - window.innerWidth;
-    }
-    this.setState({
-      displayWidth: window.innerWidth,
-      displayHeight: window.innerHeight,
-      imgLeft: imgLeft,
-      imgTop: imgTop
-    });
-  };
 
   onSearchForm = () => {
     this.props.history.push("/search");
@@ -48,15 +19,6 @@ export default class MainPage extends Component {
     console.log(this.state);
     return (
       <section className="Main">
-        <img
-          src={background}
-          className="ImageBackground"
-          alt="background"
-          style={{
-            left: `-${this.state.imgLeft}px`,
-            top: `-${this.state.imgTop}px`
-          }}
-        />
         <header className="Brown BoxShadow">
           <p className="Welcome">Добро пожаловать в</p>
           <p>торговый центр «Две пятерки»</p>

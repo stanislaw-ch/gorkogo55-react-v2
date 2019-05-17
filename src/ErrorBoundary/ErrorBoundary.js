9f4c7e9e10fd48c77868de11ddb8497d65e10fc4
Dmitry Revyakin <dm1tryrev@yandex.ru>
2019-05-17 23:32:47 +0300

    upgrade errorboundary - delete don't need fragment

--- src/ErrorBoundary/ErrorBoundary.js
+++ src/ErrorBoundary/ErrorBoundary.js
@@ -16,7 +16,7 @@ class ErrorBoundary extends Component {
     render() {
         if (this.state.hasError) {
             return <h1>{this.state.errorMessage}</h1>
-        } else return (<>{this.props.children}</>)
+        } else return this.props.children
     }
 }
 

78848f9032d0b5e659d7f21b1d8ac63bd28f3cff
reviakin <reviakindi@gmail.com>
2019-08-07 19:53:32 +0300

    ShopPage component more readeble

--- src/containers/ShopPage/ShopPage.js
+++ src/containers/ShopPage/ShopPage.js
@@ -5,58 +5,61 @@ import ShopFull from "../../components/Shop/ShopFull";
 import Header from "../../components/Header/Header";
 import Spinner from "../../components/Spinner/Spinner.tsx";
 
-const ShopPage = props => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+export default function ShopPage({
+	history,
+	match,
+	data,
+	hashTageSearch,
+	isLoadingData
+}) {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
 
-  const goSearchPage = () => {
-    props.history.push(`/search`);
-  };
+	function goSearchPage() {
+		history.push(`/search`);
+	};
 
-  const renderShop = () => {
-    const [shop] = props.data.filter(
-      shop => shop.number === props.match.params.id
-    );
-
-    const shopComponent = shop ? (
-      <ShopFull
-        hashTageSearch={props.hashTageSearch}
-        number={shop.number}
-        title={shop.title}
-        description={shop.description}
-        keywords={shop.keywords}
-        phone={shop.phone}
-        vk={shop.vk}
-        instagram={shop.instagram}
-        website={shop.website}
-        route={shop.route}
-      />
-    ) : (
-      <p>Ищем информацию...</p>
-    );
-    return shopComponent;
-  };
-
-  if (props.isLoadingData) {
-    return (
-      <Container style={{ padding: "3vh", paddingTop: "7vh" }}>
-        <Spinner />
-      </Container>
-    );
-  }
-  return (
-    <>
-      <Header
-        goBack={goSearchPage}
-        title="Горького 55"
-        history={props.history}
-      />
-      <Container style={{ padding: "3vh", paddingTop: "7vh" }}>
-        {renderShop()}
-      </Container>
-    </>
-  );
+	if (isLoadingData) {
+		return (
+			<Container style={{ padding: "3vh", paddingTop: "7vh" }}>
+				<Spinner />
+			</Container>
+		);
+	} else {
+		return (
+			<>
+				<Header
+					goBack={goSearchPage}
+					title="Горького 55"
+					history={history}
+				/>
+				<Container style={{ padding: "3vh", paddingTop: "7vh" }}>
+					{renderShop(data, match, hashTageSearch)}
+				</Container>
+			</>
+		);
+	}
 };
 
-export default ShopPage;
+function renderShop(shopList, matchRouter, hashTageSearch) {
+	var [shop] = [...shopList].filter(
+		shop => shop.number === matchRouter.params.id
+	);
+	if (shop) {
+		return <ShopFull
+			hashTageSearch={hashTageSearch}
+			number={shop.number}
+			title={shop.title}
+			description={shop.description}
+			keywords={shop.keywords}
+			phone={shop.phone}
+			vk={shop.vk}
+			instagram={shop.instagram}
+			website={shop.website}
+			route={shop.route}
+		/>
+	} else {
+		return <p>Ищем информацию...</p>
+	}
+};
